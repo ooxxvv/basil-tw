5ae993b48ed742fd3bbd560cf78ed8f0a21b1b19
ooxxvv <ooxxvv@gmail.com>
2019-10-13 21:38:23 +0800

    Remove not using code

--- 10_config_grub.sh
+++ 10_config_grub.sh
@@ -10,11 +10,8 @@ theme_path=/boot/grub/themes/basil-tw
 echo "========= Avoid the time difference between Linux & Windows ==========="
 timedatectl set-local-rtc 1
 
-echo "========= OS Icon: Remove the file that make Linux Mint as Ubuntu ====="
-sudo rm /etc/default/grub.d/50_linuxmint.cfg
-
-echo "=============== Remove old kernel ====================================="
-sudo apt autoremove --purge
+#echo "========= OS Icon: Remove the file that make Linux Mint as Ubuntu ====="
+#sudo mv /etc/default/grub.d/50_linuxmint.cfg /etc/default/grub.d/50_linuxmint.cfg.bak
 
 echo "=============== Config /etc/defalut/grub =============================="
 echo "GRUB_THEME=$theme_path/theme.txt" | sudo tee -a /etc/default/grub
@@ -23,14 +20,8 @@ echo "=============== Set lastest choice as default ========================="
 echo "GRUB_DEFAULT=saved" | sudo tee -a /etc/default/grub
 echo "GRUB_SAVEDEFAULT=true" | sudo tee -a /etc/default/grub
 
-echo "=============== Remove single user/recovery mode ======================"
-echo 'GRUB_DISABLE_RECOVERY="true"' | sudo tee -a /etc/default/grub
-
-echo "=============== Remove advanced option items =========================="
-echo 'GRUB_DISABLE_SUBMENU=y' | sudo tee -a /etc/default/grub
-
 echo "=============== Update GRUB2 Config ==================================="
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo update-grub2
 
 echo "=============== Done, please reboot system ============================"
 
